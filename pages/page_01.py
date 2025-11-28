8b6937193f655e814603e39f933c2807f66a7389
Your Name <william.descamps@gmail.com>
2025-11-28 10:19:05 +0100

    update secrets

--- pages/page_01.py
+++ pages/page_01.py
@@ -4,3 +4,7 @@ st.write("Let's get our secrets!")
 
 spell = st.secrets['spell']
 key = st.secrets.some_magic_api.key
+
+st.write(f"Our secret spell is : {spell}")
+
+st.write(f"Our secret key is : {key}")
